5379774640e07c495ca91db45c969671d8ed1d25
Serypja <100339454+Serypja@users.noreply.github.com>
2022-02-25 21:30:10 +0700

    Commit

---
+++ TestProjectZ/Creatora.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+#include"Transport.h"
+using namespace std;
+
+// Класс для возращения класса Tranport
+class Creator {
+public:
+    virtual ~Creator() { delete tranport; };
+    virtual Tranport* GetInfoTransport() const = 0;
+    virtual void DefaulTransport() = 0;
+    virtual void GetInfoTransp() = 0;
+
+    void GetSomeOperatir() { // Метод для вывода информации обьекта на экран
+        tranport->GetObjectMachine();
+    }
+
+    void SetSomeOperatir() { // метод который создаёт и заполняет дефолтными значениями
+        tranport = this->GetInfoTransport(); // Создание объекта и зануление переменных
+        tranport->SetObgectTranport(); // Объявление дефолтными переменными объекта
+    }
+
+private:
+    Tranport* tranport;
+};
+
+
+
+// как я понял эти классы должны реализовывать скрытый функцианал других классов
+//(данный класс реализовать класс Motorcycle)
+//И вся работа с классами ведётся тута). Но я с патернами знаком на уровне знания слова и для чего они нужны, так что ¯\_(ツ)_/¯
+class MotorcycleC : public Creator {
+public:
+    void DefaulTransport() override {
+        SetSomeOperatir(); // обращение к методу который создаёт и заполняет дефолтными значениями
+    }
+
+    void GetInfoTransp() override {
+        GetSomeOperatir(); // обращение к методу для вывода информации обьекта на экран
+    }
+
+    Tranport* GetInfoTransport() const override {
+        return new Motorcycle(); //Создание обьекта для дальнейшего его присвоения и работе в классе Create
+    }
+private:
+
+};
+
+class Kick_ScooterC : public Creator {
+public:
+    void DefaulTransport() {
+        SetSomeOperatir();// обращение к методу который создаёт и заполняет дефолтными значениями
+    }
+
+    void GetInfoTransp() override {
+        GetSomeOperatir();// обращение к методу для вывода информации обьекта на экран
+    }
+
+    Tranport* GetInfoTransport() const override {
+        return new Kick_Scooter();//Создание обьекта для дальнейшего его присвоения и работе в классе Create
+    }
+private:
+
+};
+
+class CarC : public Creator {
+public:
+    void DefaulTransport() {
+        SetSomeOperatir();// обращение к методу который создаёт и заполняет дефолтными значениями
+    }
+
+    void GetInfoTransp() override {
+        GetSomeOperatir();// обращение к методу для вывода информации обьекта на экран
+    }
+
+    Tranport* GetInfoTransport() const override {
+        return new Car();//Создание обьекта для дальнейшего его присвоения и работе в классе Create
+    }
+private:
+
+};
+
+class BusC : public Creator {
+public:
+    void DefaulTransport() {
+        SetSomeOperatir();// обращение к методу который создаёт и заполняет дефолтными значениями
+    }
+
+    void GetInfoTransp() override {
+        GetSomeOperatir();// обращение к методу для вывода информации обьекта на экран
+    }
+
+    Tranport* GetInfoTransport() const override {
+        return new Bus();//Создание обьекта для дальнейшего его присвоения и работе в классе Create
+    }
+private:
+
+};
+
---
+++ TestProjectZ/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+#include"Transport.h"
+#include"Creatora.h"
+using namespace std;
+
+void StartProg(Creator& cl) {
+    cl.DefaulTransport();//Создаём и объявляем переменные
+    cl.GetInfoTransp(); //Метод вывода информации на экран
+}
+
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "rus");
+    MotorcycleC MotC;
+    Kick_ScooterC KSC;
+    CarC CC;
+    BusC BC;
+
+    string* a = new string[argc];
+    if (argc > 1) {
+        for (int i = 0; i < argc; i++)
+        {
+            //cout << "argv[:" << i << "] = " << endl;  ======= Проверка вывода в консоль
+            cout << argv[i] << endl; //======= Проверка вывода в консоль
+
+            switch (stoi(argv[i]))
+            {
+            case 0:
+                StartProg(MotC);
+                break;
+            case 1:
+                StartProg(KSC);
+                break;
+            case 2:
+                StartProg(CC);
+                break;
+            case 3:
+                StartProg(BC);
+                break;
+            default:
+                cout << "НЕИЗВЕСТНЫЙ ТИП ТРАНСПОРТА." << endl;
+                break;
+            }
+        }
+    }
+    
+    delete[] a;
+    system("pause");
+
+    return 0;
+}
